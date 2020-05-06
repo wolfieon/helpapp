9a7db08162ad41e522309b04a3b460ba8cb616cf
Wolfieon <mycrafty@hotmail.se>
2020-05-06 20:06:43 +0200

    new dart obj

--- lib/ui/views/map_view.dart
+++ lib/ui/views/map_view.dart
@@ -1,3 +1,4 @@
+import 'package:compound/models/markers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong/latlong.dart';
@@ -21,39 +22,6 @@ class MapView extends StatelessWidget {
 //marker list
 final List<MarkObj> markers = [];
 
-class MarkObj {
-  GeoPoint coords;
-  String type;
-  String name;
-  String desc;
-  String userID;
-  String markerID;
-  double distance;
-
-  GeoPoint get getGeo {
-    return coords;
-  }
-
-  String get getDesc {
-    return desc;
-  }
- String get getType {
-    return type;
-  }
-  String get getName {
-    return name;
-  }
-  String get getUserID {
-    return userID;
-  }
-  String get getMarkerID {
-    return markerID;
-  }
-  double get getDistance {
-    return distance;
-  }
-  MarkObj({this.coords,this.type,this.name,this.desc,this.userID,this.markerID,this.distance,});
-}
 // ends here
 
 
