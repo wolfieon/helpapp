6fab8d94630b056fb49b0fdf6da3e0a6ecfd6d5b
Wolfieon <mycrafty@hotmail.se>
2020-05-07 16:35:24 +0200

    request working very nice!

--- lib/ui/views/need_help_view.dart
+++ lib/ui/views/need_help_view.dart
@@ -2,14 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:compound/models/chat.dart';
 import 'package:compound/services/authentication_service.dart';
 import 'package:compound/ui/shared/ui_helpers.dart';
-import 'package:compound/ui/views/startup_view.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:compound/models/markers.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong/latlong.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:compound/services/firestore_service.dart';
 
 import '../../locator.dart';
@@ -24,6 +20,29 @@ enum Services { Matvaror, Socialt, Teknisk }
 Services _services = Services.Matvaror;
 
 class _NeedHelpViewState extends State<NeedHelpView> {
+
+
+  String desc;
+  String type;
+  final databaseReference = Firestore.instance;
+  final AuthenticationService authService = locator<AuthenticationService>();
+
+  addRequest() async {
+    Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    Firestore.instance.collection('markers').add({
+      'type': type,
+      'name': 'Placeholder for name',  // GetCurrentUser Name ask philip. 
+      'desc': desc,
+      'userID': await authService.getCurrentUID(),
+      'coords':
+          new GeoPoint(position.latitude, position.longitude),
+      });
+      //Trigged flag on account here. prevent more posts.
+      //Move back to main menu?
+    } 
+
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -45,7 +64,7 @@ class _NeedHelpViewState extends State<NeedHelpView> {
         title: const Text('Matvaror'),
         value: Services.Matvaror,
         groupValue: _services,
-        onChanged: (Services value) { setState(() { _services = value; }); },
+        onChanged: (Services value) { setState(() { _services = value; type = 'Matvaror';}); },
             ),
           ),
           Align(
@@ -53,7 +72,7 @@ class _NeedHelpViewState extends State<NeedHelpView> {
         title: const Text('Socialt'),
         value: Services.Socialt,
         groupValue: _services,
-        onChanged: (Services value) { setState(() { _services = value; }); },
+        onChanged: (Services value) { setState(() { _services = value; type = 'Socialt';}); },
            ),
           ),
           Align(
@@ -61,7 +80,7 @@ class _NeedHelpViewState extends State<NeedHelpView> {
         title: const Text('Teknisk'),
         value: Services.Teknisk,
         groupValue: _services,
-        onChanged: (Services value) { setState(() { _services = value; }); },
+        onChanged: (Services value) { setState(() { _services = value; type = 'Teknisk';}); },
            ),
           ),
           SizedBox(height: 40),
@@ -84,6 +103,7 @@ class _NeedHelpViewState extends State<NeedHelpView> {
         ),
       ),
       child: new TextField(
+        onChanged: (text) {desc = text;},
         textAlign: TextAlign.center,
         decoration: new InputDecoration(
           border: InputBorder.none,
@@ -123,10 +143,7 @@ class _NeedHelpViewState extends State<NeedHelpView> {
                             fontSize: 25,
                             fontWeight: FontWeight.w600))),
           onPressed: () async {
-               await _helpLogic().listthething2();
-               await _helpLogic().sorthething();
-               await _helpLogic().hardcodedChatmetod();
-
+                await addRequest();
                },
               ),
             ),
@@ -138,94 +155,3 @@ class _NeedHelpViewState extends State<NeedHelpView> {
     );
   }
 }
-
-class _helpLogic  {
-
-final List<MarkObj> markers = [];
-final AuthenticationService authService = locator<AuthenticationService>();
-
-  final databaseReference = Firestore.instance;
-  bool listMade = false;
-  String _address = "";
-
-  void getCurrentLocation() async {
-   final position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-   print(position);
-   List<Placemark> placemark = await Geolocator().placemarkFromCoordinates(59.3293, 18.0686);
-   Placemark placeMark  = placemark[0]; 
-   String name = placeMark.name;
-   String subLocality = placeMark.subLocality;
-   String locality = placeMark.locality;
-   String administrativeArea = placeMark.administrativeArea;
-   String postalCode = placeMark.postalCode;
-   String country = placeMark.country;
-   String address = "${name}, ${subLocality}, ${locality}, ${administrativeArea} ${postalCode}, ${country}";
-   _address = address;
-   print(_address);
-   
-   //double distanceInMeters = await Geolocator().distanceBetween(52.2165157, 6.9437819, 52.3546274, 4.8285838);        Usefull later for distance
-  }
-  
-  fetchthething() {
-    databaseReference.collection("markers").getDocuments().then((snapshot) {
-      snapshot.documents.forEach((f) => print('${f.data}}'));
-    print(snapshot.documents[1].data['coords'].latitude);
-    print(snapshot.documents[1].documentID);
-    removethething(snapshot.documents[1].documentID);
-    });
-  }
-
-
-      listthething2() async {
-        QuerySnapshot snapshot = await databaseReference.collection("markers").getDocuments();
-
-        for(var f in snapshot.documents) {
-          double distanceInMeters = await Geolocator().distanceBetween(f.data['coords'].latitude, f.data['coords'].longitude, 52.3546274, 4.8285838);
-          MarkObj newMarkObj = MarkObj (coords: f.data['coords'],name: f.data['name'],desc: f.data['desc'],userID: f.data['userID'], markerID: f.documentID, distance: distanceInMeters);
-          print(distanceInMeters);
-          markers.add(newMarkObj);
-        }
-  }
-
-
-  sorthething() { // snapshot data document ID (fetchthething)
-        if (markers.length > 1) {
-          markers.sort((a, b) => a.getDistance.compareTo(b.getDistance));
-          print(markers[0].distance);
-          print('list sorted');
-      } else {
-          print('list is less than 2');
-      }
-  }
-
-
-    addthething() {
-    Firestore.instance.collection('markers').add({
-      'type': 'a type',
-      'name': 'a place',
-      'desc': 'a description',
-      'userID': 'user billy',
-      'coords':
-          new GeoPoint(42, 42),
-      });
-    }
-
-    removethething(index) { // snapshot data document ID = index (fetchthething)
-      databaseReference.collection('markers').document(index).delete();
-    }
-
-
-    dothething() async {
-      print('started');
-      double distanceInMeters = await Geolocator().distanceBetween(52.2165157, 6.9437819, 52.3546274, 4.8285838);
-      print(distanceInMeters);
-      print('stopped');
-    }
-
-
-    hardcodedChatmetod() async  { // will add closest marker to chat with you
-      Chatters nyChat = new Chatters (messengerid1: markers[0].userID, messengerid2: await authService.getCurrentUID());
-      FirestoreService().createChat(nyChat);
-    }
-}
-
